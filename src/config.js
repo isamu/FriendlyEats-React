111defa494355a7019f20d90feae56ee481fba76
Isamu Arimoto <isamu@to-kyo.to>
2019-08-16 11:55:02 +0900

    remove default firebase config

--- src/config.js
+++ src/config.js
@@ -1,12 +1,5 @@
 // Replace this section with the config file of your own Firebase project. 
 const firebaseConfig = {
-  apiKey: "AIzaSyBASj7cMKNvl_dVniIlB3KwIy95r2M258k",
-  authDomain: "friendlyeats-react.firebaseapp.com",
-  databaseURL: "https://friendlyeats-react.firebaseio.com",
-  projectId: "friendlyeats-react",
-  storageBucket: "friendlyeats-react.appspot.com",
-  messagingSenderId: "574329526997",
-  appId: "1:574329526997:web:9998626fe2cfea6c"
 };
 
 export default firebaseConfig;
